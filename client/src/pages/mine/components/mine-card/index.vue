85681543aa2fb93bf376dd3e985b87cec7b12623
cleves0315 <cleves0315@foxmail.com>
2022-10-21 01:15:50 +0800

    feat: 新增 我的 页面

---
+++ client/src/pages/mine/components/mine-card/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <view class="mine-card">
+    <view class="card-title">工具栏</view>
+    <view class="case-wrapper">
+      <view class="case-wrap" v-for="item in toolsList" :key="item.type" @click="handleClick(item.type)">
+        <image class="case-icon" mode="widthFix" :src="item.icon"></image>
+        <view class="case-text">{{ item.text }}</view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import { reactive, toRefs } from 'vue';
+import ComputerIcon from '@/assets/mine/computer.png';
+
+const data = reactive({
+  toolsList: [{ icon: ComputerIcon, text: '累计最多数', type: 'statistics' }],
+});
+
+const handleClick = (type) => {
+  switch (type) {
+    case 'statistics':
+      wx.navigateTo({
+        url: `/pages/tool/tool?type=${type}`,
+      });
+      break;
+
+    default:
+      break;
+  }
+};
+
+const { toolsList } = toRefs(data);
+</script>
+
+<style lang="scss">
+.mine-card {
+  padding: 40rpx 30rpx;
+  border-radius: 10rpx;
+  background-color: #fff;
+
+  .case-wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    margin-top: 40rpx;
+  }
+
+  .case-wrap {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  .case-icon {
+    width: 60rpx;
+  }
+
+  .case-text {
+    margin-top: 10rpx;
+    font-size: 24rpx;
+    color: #838383;
+  }
+}
+</style>
---
+++ client/src/pages/mine/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="mine-page">
+    <MineHeader />
+    <view class="card-wrapper">
+      <MineCard />
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import MineHeader from './components/mine-header/index.vue';
+import MineCard from './components/mine-card/index.vue';
+</script>
+
+<style lang="scss">
+.mine-page {
+  min-height: 100vh;
+  overflow: hidden;
+  background-color: #f8f8f8;
+
+  .card-wrapper {
+    transform: translate(35rpx, -35rpx);
+  }
+}
+</style>
