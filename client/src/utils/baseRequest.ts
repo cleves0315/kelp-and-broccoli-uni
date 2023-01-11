84a5d28b31aee9d236c866aba063ca07bd4fd920
cleves0315 <cleves0315@foxmail.com>
2023-01-11 22:03:53 +0800

    feat: baseRequest 增加异常处理

--- client/src/utils/baseRequest.ts
+++ client/src/utils/baseRequest.ts
@@ -16,7 +16,7 @@ export const baseRequest = async <ResultData = IResData>(params: RQ<ICloud.CallF
   return wx.cloud
     .callFunction(params)
     .then((res) => {
-      const { errMsg, result } = res as { errMsg: string; result: IRequestResult };
+      const { result } = res as { errMsg: string; result: IRequestResult };
 
       if (result.code === 1) {
         groupCollapsed(`request ${params.name} -> success`);
@@ -30,6 +30,9 @@ export const baseRequest = async <ResultData = IResData>(params: RQ<ICloud.CallF
       }
     })
     .catch((err) => {
+      if (err?.errCode === -1) {
+        return Promise.reject({ code: -1, message: '网络请求失败' });
+      }
       return Promise.reject({ code: -1, message: err.message || '加载失败' });
     });
 };
