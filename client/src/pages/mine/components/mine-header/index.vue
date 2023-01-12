bae9c2a10cbb7e81081b62091dc69c253964bce4
cleves0315 <cleves0315@foxmail.com>
2023-01-12 11:51:00 +0800

    style: 我的页面 logo 图片优化

--- client/src/pages/mine/components/mine-header/index.vue
+++ client/src/pages/mine/components/mine-header/index.vue
@@ -1,15 +1,15 @@
 <template>
   <view class="mine-header">
     <view class="welcome-wrapper">
-      <image
-        class="logo-icon"
-        mode="widthFix"
-        src="https://6f6e-on-line-1gqban3ba49e3d35-1302613116.tcb.qcloud.la/broccoli.png?sign=7b6211e60087fd8fb1909cca697cb0a3&t=1612709339"
-      ></image>
+      <image class="logo-icon" mode="widthFix" :src="LOGO_TRANSP_BASE64"></image>
       <text class="welcome-words">欢迎来到海带与西兰花</text>
     </view>
   </view>
 </template>
+
+<script lang="ts" setup>
+import { LOGO_TRANSP_BASE64 } from '@/constants';
+</script>
 
 <style lang="scss">
 .mine-header {
